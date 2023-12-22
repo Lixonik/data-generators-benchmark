a0b122114c4a1300d8af171c13b8be7006ed53c5
Lixonik <deimos.75@mail.ru>
2023-12-22 18:16:13 +0300

    from stackblitz

--- src/bencmark/benchmark.ts
+++ src/bencmark/benchmark.ts
@@ -1,6 +1,7 @@
 import { performance } from 'perf_hooks'
-import * as fs from 'fs'
-import path from 'path'
+import { Chart } from 'chart.js';
+import * as fs from 'fs';
+import { createCanvas } from 'canvas';
 
 export class Benchmark {
     static generatorFunctions: Function[] = []
@@ -10,7 +11,12 @@ export class Benchmark {
     }
 
     static measureTime() {
+
+
         Benchmark.generatorFunctions.forEach(fn => {
+
+            const x: number[] = []
+            const y: number[] = []
 
             for (let i = 0; i < 999; i++) {
                 const start = performance.now()
@@ -18,9 +24,60 @@ export class Benchmark {
                 const end = performance.now()
                 const duration = end - start
 
+                x.push(i)
+                y.push(duration)
+
+                // Создайте элемент canvas в памяти с помощью jsdom
+                const canvas = createCanvas(800, 600);
+                const ctx = canvas.getContext('2d');
+
+                // Настройки графика
+                const chartConfig = {
+                    type: 'line',
+                    data: {
+                        labels: x.map(String),
+                        datasets: [{
+                            label: 'График зависимости y от x',
+                            data: y,
+                            borderColor: 'blue',
+                            borderWidth: 2,
+                            fill: false,
+                        }],
+                    },
+                    options: {
+                        scales: {
+                            x: {
+                                type: 'linear',
+                                position: 'bottom',
+                                title: {
+                                    display: true,
+                                    text: 'Значения x',
+                                },
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Значения y',
+                                },
+                            },
+                        },
+                    },
+                };
+
+                // Создайте график на основе настроек
+                //@ts-ignore
+                const chart = new Chart(ctx, chartConfig);
+
+                // Сохраните график в файл .png
+                const fileName = 'plot.png';
+                const out = fs.createWriteStream(fileName);
+                const stream = canvas.createPNGStream();
+                stream.pipe(out);
+                out.on('finish', () => console.log(`График сохранен в файл: ${fileName}`));
+
                 //console.log(`Время выполнения функции ${fn.name}: ${duration} миллисекунд, result = ${result}`)
 
-                fs.writeFileSync(path.join(process.cwd(), `/measurements/strings/${fn.name}.txt`), `${i} ${duration}\n`, { flag: 'a+' })
+                // fs.writeFileSync(path.join(process.cwd(), `/measurements/strings/${fn.name}.txt`), `${i} ${duration}\n`, { flag: 'a+' })
             }
         })
     }
