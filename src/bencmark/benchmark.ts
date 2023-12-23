c3f44422c2a589aba19b496b6485b98d85ec226c
Prokhorov Boris <deimos.75@mail.ru>
2023-12-23 19:19:37 +0300

    - Added plt charts
    Need to:
    - avg

--- src/bencmark/benchmark.ts
+++ src/bencmark/benchmark.ts
@@ -1,85 +1,68 @@
 import { performance } from 'perf_hooks'
-import { Chart } from 'chart.js';
-import * as fs from 'fs';
-import { createCanvas } from 'canvas';
+import { ChartConfiguration, ChartData, ChartItem } from 'chart.js'
+import { Chart } from 'chart.js/auto'
+import * as fs from 'fs'
+import { PathLike } from 'fs'
+import { createCanvas } from 'canvas'
 
 export class Benchmark {
     static generatorFunctions: Function[] = []
 
-    static pushCandidate = (candidate: Function) => {
-        Benchmark.generatorFunctions.push(candidate)
+    static pushCandidate = (...candidates: Function[]) => {
+        Benchmark.generatorFunctions.push(...candidates)
     }
 
-    static measureTime() {
+    static measureTime(pathToMeasurementsPng?: PathLike) {
+        Benchmark.generatorFunctions.forEach((fn) => {
+            const generatedStringLength: number[] = []
+            const timeToGeneration: number[] = []
 
-
-        Benchmark.generatorFunctions.forEach(fn => {
-
-            const x: number[] = []
-            const y: number[] = []
-
-            for (let i = 0; i < 999; i++) {
+            for (let i = 0; i < 99; i++) {
                 const start = performance.now()
                 fn(i)
                 const end = performance.now()
                 const duration = end - start
 
-                x.push(i)
-                y.push(duration)
-
-                // Создайте элемент canvas в памяти с помощью jsdom
-                const canvas = createCanvas(800, 600);
-                const ctx = canvas.getContext('2d');
-
-                // Настройки графика
-                const chartConfig = {
-                    type: 'line',
-                    data: {
-                        labels: x.map(String),
-                        datasets: [{
-                            label: 'График зависимости y от x',
-                            data: y,
-                            borderColor: 'blue',
-                            borderWidth: 2,
-                            fill: false,
-                        }],
-                    },
-                    options: {
-                        scales: {
-                            x: {
-                                type: 'linear',
-                                position: 'bottom',
-                                title: {
-                                    display: true,
-                                    text: 'Значения x',
-                                },
-                            },
-                            y: {
-                                title: {
-                                    display: true,
-                                    text: 'Значения y',
-                                },
-                            },
-                        },
-                    },
-                };
-
-                // Создайте график на основе настроек
-                //@ts-ignore
-                const chart = new Chart(ctx, chartConfig);
-
-                // Сохраните график в файл .png
-                const fileName = 'plot.png';
-                const out = fs.createWriteStream(fileName);
-                const stream = canvas.createPNGStream();
-                stream.pipe(out);
-                out.on('finish', () => console.log(`График сохранен в файл: ${fileName}`));
-
-                //console.log(`Время выполнения функции ${fn.name}: ${duration} миллисекунд, result = ${result}`)
-
-                // fs.writeFileSync(path.join(process.cwd(), `/measurements/strings/${fn.name}.txt`), `${i} ${duration}\n`, { flag: 'a+' })
+                generatedStringLength.push(i)
+                timeToGeneration.push(duration)
             }
+
+            const width = 1200, height = 800
+            const canvas = createCanvas(width, height)
+            const ctx = canvas.getContext('2d') as unknown as ChartItem
+            const plugin = {
+                id: 'customCanvasBackgroundImage',
+                beforeDraw: (chart: Chart) => {
+                    const ctx = chart.ctx
+                    ctx.fillStyle = '#ffffff'
+                    ctx.fillRect(0, 0, canvas.width, canvas.height)
+                },
+            }
+            const data: ChartData = {
+                labels: generatedStringLength.map(String),
+                datasets: [
+                    {
+                        label: 'Line generation time vs length graph',
+                        data: timeToGeneration,
+                        borderColor: 'blue',
+                        borderWidth: 2,
+                        fill: false,
+                    },
+                ],
+            }
+            const config: ChartConfiguration = {
+                type: 'line',
+                data: data,
+                plugins: [plugin],
+            }
+            new Chart(ctx, config)
+            const buffer = canvas.toBuffer('image/png')
+
+            if (!fs.existsSync(`${pathToMeasurementsPng ?? './measurements'}`)) {
+                fs.mkdirSync(`${pathToMeasurementsPng ?? './measurements'}`)
+            }
+
+            fs.writeFileSync(`${pathToMeasurementsPng ?? './measurements'}/${fn.name}.png`, buffer)
         })
     }
-
 }
